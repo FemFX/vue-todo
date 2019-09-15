bf7ad06c7f008be6bd71e7abecd775b2cf82d579
FemFX <ignedko34@gmail.com>
2019-09-15 07:47:21 +0300

    components

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+    <input type="text" name="todo" id="todo" v-model="todoValue" placeholder="Your Todo" />
+    <button id="createTodo" class="submit" @click="createTodo">+</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      todoValue: ""
+    };
+  },
+  methods: {
+    createTodo: function() {
+      if (this.todoValue) {
+        this.$emit("createTodo", this.todoValue);
+        this.todoValue = "";
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+}
+header input {
+  flex: 1;
+  height: 50px;
+  box-sizing: border-box;
+  text-indent: 15px;
+  font-size: 20px;
+}
+header input {
+  outline: none;
+}
+header .submit {
+  width: 50px;
+  height: 50px;
+  color: #171717;
+  font-size: 28px;
+  line-height: 1;
+  font-weight: 700;
+  background-color: #ffce00;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+header .submit:hover {
+  background-color: #ffd833;
+}
+header .submit:active {
+  background-color: #e6b900;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,48 @@
+<template>
+  <section id="todos">
+    <div class="todo" v-for="(todo) in todos" v-bind:key="todo.id">
+      <input type="checkbox" name="done" v-if="todo.done" checked />
+      <input type="checkbox" name="done" v-else />
+      <label for="done">{{ todo.name }}</label>
+      <button class="remove" v-on:click="() => removeTodo(todo.id)">Remove</button>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "Todos",
+  props: ["todos"],
+  methods: {
+    removeTodo: function(id) {
+      this.$emit("removeTodo", id);
+    }
+  }
+};
+</script>
+<style scoped>
+#todos {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+}
+#todos .todo {
+  width: 24%;
+  margin: 15px;
+  padding: 15px;
+  background: #fff;
+  display: flex;
+}
+#todos .todo label {
+  flex: 1;
+}
+button {
+  border: none;
+  background: #ea2027;
+  color: #fff;
+  border-radius: 5%;
+  cursor: pointer;
+  outline: none;
+}
+</style>
